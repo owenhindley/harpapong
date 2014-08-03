8390529c1b83d890eaf2726588cef78d63b5de6f
Owen Hindley <owenhindley@hotmail.com>
2014-08-03 02:38:07 +0100

    front-end styling & queue server improvements

--- deploy/common/Utils.js
+++ deploy/common/Utils.js
@@ -21,6 +21,52 @@
 			return returnObject;
 		};
 
+		
+	Utils.setCookie = function(aCookieName, aCookieValue, aExDays, aExMins, aDomain, aPath) {
+		// console.log("aCookieName, aCookieValue, aExDays, aDomain : ", aCookieName, aCookieValue, aExDays, aExMins, aDomain);
+		var exdate=new Date();
+		var hasExpire = false;
+		if(aExDays !== undefined) {
+			exdate.setDate(exdate.getDate() + aExDays);
+			hasExpire = true;
+		} 
+		if(aExMins !== undefined) {
+			exdate.setMinutes(exdate.getMinutes() + aExMins);
+			hasExpire = true;
+		}
+		
+		var aCookieValue=escape(aCookieValue) + ((hasExpire==false) ? "" : "; expires="+exdate.toUTCString()) + ((aDomain==undefined) ? "" : "; domain="+aDomain)+ ((aPath==undefined) ? "" : "; path="+aPath);
+		document.cookie=aCookieName + "=" + aCookieValue;
+	};
+
+	Utils.getCookie = function(c_name) {
+		// console.log("c_name : ", c_name);
+		var c_value = document.cookie;
+		var c_start = c_value.indexOf(" " + c_name + "=");
+		if (c_start == -1) {
+			c_start = c_value.indexOf(c_name + "=");
+		}
+		
+		if (c_start == -1) {
+			c_value = null;
+		} else {
+			c_start = c_value.indexOf("=", c_start) + 1;
+			var c_end = c_value.indexOf(";", c_start);
+			if (c_end == -1) {
+				c_end = c_value.length;
+			}
+			c_value = unescape(c_value.substring(c_start,c_end));
+		}
+		return c_value;
+	};
+
+	Utils.delete_cookie = function(name, aDomain, aPath) {
+		console.log("delete_cookie :: aDomain : ", aDomain);
+		var cookieDetails = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;' + ( (aDomain==undefined) ? "" : "domain="+aDomain+";") + ((aPath==undefined) ? "" : " path="+aPath);
+		// alert("cookieDetails : "+ cookieDetails);
+		document.cookie = cookieDetails;
+	};
+
 	global.Utils = (global.module || {}).exports = Utils;
 
 })(this);
---
+++ deploy/site/js/QueueKeeper.js
@@ -0,0 +1,172 @@
+(function() {
+
+	
+	var QueueKeeper = {
+
+		positionCallback : null,
+		joinCallback : null,
+		errorCallback : null,
+		serverURL : "",
+		queueId : null,
+
+		init : function(server, onPosition, onJoin, on_Error) {
+
+			this.serverURL = server;
+
+			this.positionCallback = onPosition;
+			this.joinCallback = onJoin;
+			this.errorCallback = on_Error;
+
+
+			return this;
+		},
+
+		joinQueue : function() {
+
+
+			// check cookies to see if we've already got an ID
+			var guid_cookie_value = Utils.getCookie("queueId");
+			if (guid_cookie_value){
+				console.log("found cookie value " + guid_cookie_value);
+				this.queueId = guid_cookie_value;
+
+				this.checkPosition();
+				
+			} else {
+				var url = this.serverURL + "/?method=join";
+				this._makeRequest(url, function(data){
+
+					if (data.status == "OK"){
+
+						var guid = data.data.id;
+						console.log("joined queue with id " + guid);
+						this.queueId = guid;
+
+						try {
+							Utils.setCookie("queueId", this.queueId);
+						} catch(e){
+
+						}
+
+						this.checkPosition();
+
+					} else {
+						this.onError(data.message);
+					}
+
+				}.bind(this), function(error){
+
+					this._resetQueuePosition();
+
+				}.bind(this));
+			}
+
+
+		},
+
+		checkPosition : function() {
+
+			var url = this.serverURL + "/?method=position&id=" + this.queueId;
+
+			this._makeRequest(url, function(data){
+
+				if (data.status == "OK"){
+					if (data.playing){
+
+						onJoinGame(data.data.playerId);
+						Utils.delete_cookie("queueId");
+
+					} else {
+
+						onPosition(data.data.position);
+
+						setTimeout(this.checkPosition.bind(this), 2000);
+
+					}
+				} else {
+					this.onError(data.message);
+				}
+
+			}.bind(this), function(error){
+
+				// handle error
+				// and re-join the queue
+				
+				this._resetQueuePosition();
+
+			}.bind(this));
+			
+		
+		},
+
+		onPosition : function(aPosition){
+
+			if (this.positionCallback){
+				this.positionCallback(aPosition);
+			}
+
+		},
+
+		onJoinGame : function(aPlayerId){
+
+			if (this.joinCallback){
+				this.joinCallback.call(aPlayerId);
+			}
+
+		},
+
+		onError : function(aError){
+
+			console.log("ERROR getting position from server : " + aError);
+			if (errorCallback){
+				errorCallback.call(this, aError);
+			}
+
+		},
+
+		_resetQueuePosition : function() {
+
+			Utils.delete_cookie("queueId");
+			this.queueId = null;
+			setTimeout(this.joinQueue.bind(this), 1000);
+		},
+
+		_makeRequest : function(aUrl, aHandleResponse, aHandleError){
+
+			var rq = new XMLHttpRequest();
+			rq.open("GET", aUrl, true);
+			rq.onreadystatechange = function(){
+				switch(rq.readyState) {
+					case 0: //Uninitialized
+					case 1: //Set up
+					case 2: //Sent
+					case 3: //Partly done
+						//: do nothing
+						break;
+					case 4: //Done
+						if(rq.status < 400) {
+							try	{
+								var data = JSON.parse(rq.responseText);
+								aHandleResponse(data);
+							} catch(err) {
+								console.error("ERROR parsing json");
+								aHandleError("ERROR parsing json");
+								break;
+							}
+							
+						}
+						break;
+				}
+
+			}
+			rq.send(null);
+
+		}
+
+
+	};
+
+	window.QueueKeeper = QueueKeeper;
+
+
+})();
